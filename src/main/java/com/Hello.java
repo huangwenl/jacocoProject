aabfd50628586f12e2327f9dc41475d1c3b54dcf
huangwenl <872222458@qq.com>
2019-05-15 18:03:11 +0800

    first commit

---
+++ src/main/java/com/Hello.java
@@ -0,0 +1,30 @@
+package com;
+
+public class Hello {
+    public int i;
+
+    public Hello() {
+
+    }
+
+    public Hello(int i) {
+        this.i = i;
+    }
+
+    public String testMethod1() {
+        return "Hello World";
+    }
+
+    public int addMethod(int a, int b) {
+        if (a > b) {
+            int c = 5;
+            return c;
+        }
+        return a + b;
+    }
+
+    public String toString() {
+        return String.valueOf(this.i);
+    }
+
+}
---
+++ src/test/java/HelloTest.java
@@ -0,0 +1,34 @@
+import org.junit.Assert;
+import org.junit.Test;
+import com.Hello;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class HelloTest {
+
+    @Test
+    public void testMethod1() {
+        Hello hw = new Hello();
+        String ss = hw.testMethod1();
+        Assert.assertNotNull(ss);
+    }
+
+    @Test
+    public void testMethod2() {
+        Hello hw = new Hello();
+        int ss = hw.addMethod(1, 1);
+        Assert.assertEquals(ss, 2);
+    }
+
+    @Test
+    public void testMethod2_1() {
+        Hello hw = new Hello(2);
+        ArrayList l = new ArrayList();
+        l.add(hw);
+        System.out.print(l);
+        int ret = hw.addMethod(10, 9);
+        Assert.assertEquals(ret, 5);
+    }
+}
