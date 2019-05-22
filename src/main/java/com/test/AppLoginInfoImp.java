e3639190c6977d7a97cab31713d662037c9c91c9
huangwenl <872222458@qq.com>
2019-05-22 14:19:16 +0800

    five commit "and gentrandint method"

--- src/main/java/com/test/AppLoginInfoImp.java
+++ src/main/java/com/test/AppLoginInfoImp.java
@@ -2,6 +2,8 @@ package com.test;
 
 import com.User;
 
+import java.util.Random;
+
 public class AppLoginInfoImp implements App_Login_Info {
     public void getNum(int numbers) {
         int num = 100;
@@ -12,5 +14,9 @@ public class AppLoginInfoImp implements App_Login_Info {
 
     public void getUserInfo(User user) {
         System.out.print(user.getUsername());
+    }
+
+    public Integer getRandInt() {
+        return new Random().nextInt();
     }
 }
